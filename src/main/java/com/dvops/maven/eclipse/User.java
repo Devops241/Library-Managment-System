531a30326d711cecc41dd682db1ccfe0bbb1cf37
Devops241 <157200402+Devops241@users.noreply.github.com>
2024-02-01 17:42:52 +0800

    Update User.java
    Update the user to usernamelibrary - HK reviewed

--- src/main/java/com/dvops/maven/eclipse/User.java
+++ src/main/java/com/dvops/maven/eclipse/User.java
@@ -3,7 +3,7 @@ package com.dvops.maven.eclipse;
 public class User {
 	public User(String username, String password) {
 		super();
-		this.username = username;
+		this.username = usernamelibrary;
 		this.password = password;
 	}
 	protected String username;
