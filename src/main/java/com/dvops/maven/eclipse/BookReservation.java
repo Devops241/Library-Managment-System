66fb5d21d044b4699d59eb46bccd45e85948da01
Devops241 <huongkieu.le@gmail.com>
2024-02-01 16:28:17 +0800

    Add in Book Reservation Function

---
+++ src/main/java/com/dvops/maven/eclipse/BookReservation.java
@@ -0,0 +1,14 @@
+package com.dvops.maven.eclipse;
+
+public class BookReservation {
+	public BookReservation(String username, String password) {
+		super();
+		this.username = username;
+		this.password = password;
+	}
+	protected String username;
+	protected String password;
+	
+
+
+}
